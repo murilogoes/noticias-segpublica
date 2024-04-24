74847ff7e6efbaf1100abd7729bd262420e1db87
Murilo Goes de Almeida <murilo.almeida@mj.gov.br>
2024-04-24 13:59:00 -0300

    subindo nova versao

--- app.py
+++ app.py
@@ -3,32 +3,11 @@ from flask_cors import CORS
 import requests
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
-import re
-from GoogleNews import GoogleNews
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 CORS(app)
-
-allowed_media = [
-    "Jovem Pan", "SBT", "Jornal Correio", "Valor Econômico", "Band Jornalismo",
-    "Terra", "Brasil 247", "CNN Brasil", "O Globo", "Estadão", "Poder360",
-    "Portal iG", "CartaCapital", "Crusoé", "Metrópoles", "G1", "O Antagonista",
-    "Jornal de Brasília", "Hora do Povo", "Gazeta de S. Paulo", "UOL", "VEJA",
-    "Gazeta do Povo", "Revista Oeste"
-]
-
-def extrair_horas(texto):
-    if 'hora' in texto:
-        match = re.search(r'(\d+)\s+hora', texto)
-        if match:
-            return int(match.group(1))
-    elif 'ontem' in texto:
-        return 24  # Considerar 'ontem' como 24 horas atrás
-    elif 'dias' in texto:
-        match = re.search(r'(\d+)\s+dia', texto)
-        if match:
-            return int(match.group(1)) * 24
-    return float('inf')  # Para texto que não corresponde a nenhum padrão
 
 @app.route("/") 
 def hello():     
@@ -71,25 +50,62 @@ def get_noticias():
                 link_completo = href if href.startswith('http') else base_url + href
                 noticias["senado"].append({"titulo": titulo, "link": link_completo})
 
-    # Notícias do Google News
-    googlenews = GoogleNews(lang='pt-BR', region='BR', period='1d')
-    googlenews.get_news('seguranca publica')
-    results_gnews = googlenews.results()
 
-    def extrair_horas(texto):
-        match = re.search(r'(\d+)\s+hora', texto)
-        if match:
-            return int(match.group(1))
-        return 0
 
-    dados_ordenados = sorted(results_gnews, key=lambda x: extrair_horas(x['date']))
-    for item in dados_ordenados:
-        media = item['media'] 
-        if media in allowed_media:                       
-            date = item['date']       
-            title =  item['title']
-            link = 'https://' + item['link']
-            noticias["google_news"].append({"titulo": title, "link": link, "midia": media, "data": date})
+
+
+    # Suponha que você fez uma requisição GET para a URL
+    response = requests.get("https://news.google.com/rss/search?hl=pt-BR&gl=BR&ceid=BR%3Apt-419&oc=11&q=policia%20militar%20when%3A1d")
+    xml_content = response.text
+
+    # Parse do conteúdo XML
+    root = ET.fromstring(xml_content)
+
+    news_items = []
+
+    for item in root.findall('.//item'):
+        title = item.find('title').text
+        link = item.find('link').text
+        pub_date = item.find('pubDate').text
+        data_obj = datetime.strptime(pub_date, '%a, %d %b %Y %H:%M:%S GMT').replace(tzinfo=pytz.utc)
+
+        # Definir o fuso horário de Brasília (UTC-3)
+        fuso_brasilia = pytz.timezone('America/Sao_Paulo')
+
+        # Converter para o fuso horário de Brasília
+        data_brasilia = data_obj.astimezone(fuso_brasilia)
+
+        # Formatar para o estilo brasileiro
+        data_formatada = data_brasilia.strftime('%d/%m/%Y %H:%M:%S')
+        source = item.find('source').text
+
+        news_items.append({
+            'title': title,
+            'link': link,
+            'pub_date': data_formatada,
+            'source': source
+        })
+
+    # Ordenar notícias pela data mais recente
+    news_items.sort(key=lambda x: x['pub_date'], reverse=True)
+    
+    # Converter string para objeto datetime com GMT
+
+
+
+    # Dicionário para armazenar as notícias
+    #noticias = {"google_news": []}
+
+    # Adicionar ao dicionário
+    for item in news_items:
+        media = item['source']
+        date = item['pub_date']
+        title = item['title']
+        link = item['link']
+        noticias["google_news"].append({"titulo": title, "link": link, "midia": media, "data": date})
+
+    # Mostrar o resultado
+    #print(noticias)
 
     return jsonify(noticias)
 
